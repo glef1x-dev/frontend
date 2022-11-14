9fa94c8291df1bde5fb9f1071cc048e75e9f9b5c
GLEF1X <glebgar567@gmail.com>
2022-11-13 21:48:28 -0500

    [experimental]: memoize rendered article body markdown for the sake of performance

--- app/routes/Blog/BlogArticle.tsx
+++ app/routes/Blog/BlogArticle.tsx
@@ -9,11 +9,60 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { default as atomDarkStyle } from "react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark";
 import ScrollProgressBar from "../../common/ScrollProgressBar.js";
 import { usePageEffect } from "../../core/page.js";
+import { useMemo } from "react";
 
 export default function BlogArticle() {
   const article = useLoaderData() as Article;
 
   usePageEffect({ title: article.title });
+  const renderedArticleBodyAsMarkdown = useMemo(() => {
+    return (
+      <ReactMarkdown
+        children={article.body}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <Box
+                sx={{
+                  "& code": {
+                    fontFamily: "FantasqueSansMonoRegular",
+                    fontSize: "1em",
+                    letterSpacing: ".9px",
+                    fontWeight: "bold",
+                    "@media(max-width: 600px)": {
+                      fontSize: "0.8em",
+                      letterSpacing: "0",
+                    },
+                  },
+                }}
+              >
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, "")}
+                  language={match[1]}
+                  // PreTag="div"
+                  style={atomDarkStyle}
+                  wrapLines="true"
+                  wrapLongLines="true"
+                  remarkPlugins={[remarkGfm]}
+                  rehypePlugins={[rehypeRaw]}
+                  customStyle={{
+                    borderRadius: "10px",
+                    padding: "20px",
+                  }}
+                  {...props}
+                />
+              </Box>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    );
+  }, [article.body]);
 
   return (
     <div>
@@ -65,50 +114,7 @@ export default function BlogArticle() {
               }}
             />
           </header>
-          <ReactMarkdown
-            children={article.body}
-            components={{
-              code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || "");
-                return !inline && match ? (
-                  <Box
-                    sx={{
-                      "& code": {
-                        fontFamily: "FantasqueSansMonoRegular",
-                        fontSize: "1em",
-                        letterSpacing: ".9px",
-                        fontWeight: "bold",
-                        "@media(max-width: 600px)": {
-                          fontSize: "0.8em",
-                          letterSpacing: "0",
-                        },
-                      },
-                    }}
-                  >
-                    <SyntaxHighlighter
-                      children={String(children).replace(/\n$/, "")}
-                      language={match[1]}
-                      // PreTag="div"
-                      style={atomDarkStyle}
-                      wrapLines="true"
-                      wrapLongLines="true"
-                      remarkPlugins={[remarkGfm]}
-                      rehypePlugins={[rehypeRaw]}
-                      customStyle={{
-                        borderRadius: "10px",
-                        padding: "20px",
-                      }}
-                      {...props}
-                    />
-                  </Box>
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-          />
+          {renderedArticleBodyAsMarkdown}
         </article>
       </Container>
     </div>
