526523977a96864c6c1e5c5ac9b4f044696546a2
GLEF1X <glebgar567@gmail.com>
2022-11-24 11:55:54 -0500

    [refactoring]: change default prism code styles to one-dark

--- app/features/blog/components/Markdown.tsx
+++ app/features/blog/components/Markdown.tsx
@@ -1,8 +1,7 @@
 import ReactMarkdown from "react-markdown";
-import { Box } from "@mui/material";
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import { default as materialDark } from "react-syntax-highlighter/dist/esm/styles/prism/material-dark";
-import { default as materialLight } from "react-syntax-highlighter/dist/esm/styles/prism/material-light";
+import { default as oneDark } from "react-syntax-highlighter/dist/esm/styles/prism/one-dark";
+import { default as oneLight } from "react-syntax-highlighter/dist/esm/styles/prism/one-light";
 import { memo } from "react";
 import { useTheme } from "@/lib/ui/mui/theme.js";
 
@@ -17,8 +16,8 @@ type MarkdownProps = {
 function Markdown(props: MarkdownProps) {
   const text = props.text;
   const prismCodeStyles = props.prismCodeStyles ?? {
-    dark: materialDark,
-    light: materialLight,
+    dark: oneDark,
+    light: oneLight,
   };
   const theme = useTheme();
   const isDarkThemeEnabled = theme.palette.mode === "dark";
@@ -26,42 +25,37 @@ function Markdown(props: MarkdownProps) {
   return (
     <ReactMarkdown
       children={text}
-      className="markdown"
+      className="markdown-text"
       components={{
         code({ node, inline, className, children, ...props }) {
           const guessedLanguageName = /language-(\w+)/
             .exec(className || "")
             ?.at(1);
           return !inline && guessedLanguageName ? (
-            <Box
-              sx={{
-                "& code": {
-                  fontFamily: "JetbrainsMonoNL, monospace",
-                  fontSize: "1em",
-                  letterSpacing: ".9px",
-                  fontWeight: "bold",
-                  "@media(max-width: 600px)": {
-                    fontSize: "0.8em",
-                    letterSpacing: "0",
-                  },
+            <SyntaxHighlighter
+              children={String(children).replace(/\n$/, "")}
+              language={guessedLanguageName}
+              style={
+                isDarkThemeEnabled
+                  ? prismCodeStyles.dark
+                  : prismCodeStyles.light
+              }
+              customStyle={{
+                borderRadius: "10px",
+                padding: "20px",
+                fontFamily: "JetbrainsMonoNL, monospace",
+                fozntWeight: "bold",
+              }}
+              codeTagProps={{
+                style: {
+                  fontFamily: "inherit",
+                  letterSpacing: "inherit",
+                  fontWeight: "inherit",
+                  fontSize: "inherit",
                 },
               }}
-            >
-              <SyntaxHighlighter
-                children={String(children).replace(/\n$/, "")}
-                language={guessedLanguageName}
-                style={
-                  isDarkThemeEnabled
-                    ? prismCodeStyles.dark
-                    : prismCodeStyles.light
-                }
-                customStyle={{
-                  borderRadius: "10px",
-                  padding: "20px",
-                }}
-                {...props}
-              />
-            </Box>
+              {...props}
+            />
           ) : (
             <code className={className} {...props}>
               {text}
