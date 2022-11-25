a9b54c22414225f60bb4c95e1181cb3a22c65f9e
GLEF1X <glebgar567@gmail.com>
2022-11-25 11:00:57 -0500

    [error handling]: add error page

---
+++ app/components/ErrorPage.tsx
@@ -0,0 +1,55 @@
+import { usePageEffect } from "@/hooks/page.js";
+import {
+  Button,
+  createTheme,
+  responsiveFontSizes,
+  Typography,
+} from "@mui/material";
+import { Box, ThemeProvider } from "@mui/system";
+import { Link, useRouteError } from "react-router-dom";
+
+interface RouterError {
+  status?: number;
+  statusText?: string;
+  data: object | null;
+}
+
+const _theme = responsiveFontSizes(
+  createTheme({
+    typography: {
+      h1: {
+        fontSize: "3rem",
+      },
+    },
+  })
+);
+
+function ErrorPage(): JSX.Element {
+  const error = useRouteError() as RouterError;
+  const errorStatus = error.status ?? 500;
+  const statusText = error.statusText ?? "Server internal error";
+  usePageEffect({ title: statusText });
+
+  return (
+    <ThemeProvider theme={_theme}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          minHeight: "100vh",
+          gap: "1rem",
+          padding: "1rem",
+        }}
+      >
+        <Typography variant="h1">{errorStatus + " " + statusText}</Typography>
+        <Button variant="contained" size="large" component={Link} to="/">
+          Back Home
+        </Button>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export { ErrorPage };
--- app/routes/router.tsx
+++ app/routes/router.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import ErrorPage from "./routing-error-page.js";
 import { getPostArticleBySlug } from "@/features/blog/api/index.js";
 import { withSuspense } from "../utils/suspense-wrapper.js";
+import { ErrorPage } from "@/components/ErrorPage.js";
 
-const Layout = withSuspense(React.lazy(() => import("../components/Layout.js")));
-const About = withSuspense(React.lazy(() => import("../features/about-me/components/About.js")));
+const Layout = withSuspense(
+  React.lazy(() => import("../components/Layout.js"))
+);
+const About = withSuspense(
+  React.lazy(() => import("../features/about-me/components/About.js"))
+);
 const OpenSource = withSuspense(
-  React.lazy(() => import("../features/opensource-projects/components/OpenSource.js"))
+  React.lazy(
+    () => import("../features/opensource-projects/components/OpenSource.js")
+  )
 );
 const Blog = withSuspense(
-  React.lazy(() => import("../features/blog/components/BlogArticlePreviewList.js"))
+  React.lazy(
+    () => import("../features/blog/components/BlogArticlePreviewList.js")
+  )
 );
 const BlogArticle = withSuspense(
   React.lazy(() => import("../features/blog/components/BlogArticle.js"))
