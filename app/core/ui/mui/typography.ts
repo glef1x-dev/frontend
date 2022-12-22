5c517f26388f9707e213477aa84b4b0e36780cd6
GLEF1X <glebgar567@gmail.com>
2022-12-22 17:56:42 -0500

    [UI]: change font-family

--- app/core/ui/mui/typography.ts
+++ app/core/ui/mui/typography.ts
@@ -1,7 +1,7 @@
 import { TypographyVariantsOptions } from "@mui/material/styles";
 
 export const options: TypographyVariantsOptions = {
-  fontFamily: ["-apple-system", "Epilogue", "sans-serif"].join(","),
+  fontFamily: ["-apple-system", "system-ui", "SegoeUI", "sans-serif"].join(","),
   fontWeightBold: "600",
   fontWeightLight: "400",
   fontWeightMedium: "500",
