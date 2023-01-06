0c2a6a4978e818526ac5cde217fff92a08a90963
GLEF1X <glebgar567@gmail.com>
2023-01-05 21:53:32 -0500

    Revert "[bugfix]: prefer user preferences over stored value"
    This reverts commit d38c3187a35bf74d536966fc12604c9438aa6caf.

--- app/core/ui/mui/theme.ts
+++ app/core/ui/mui/theme.ts
@@ -20,12 +20,14 @@ export const ThemeName = atom<PaletteMode>({
       const storageKey = "theme";
 
       if (ctx.trigger === "get") {
-        const name: PaletteMode = matchMedia?.("(prefers-color-scheme: dark)")
-          .matches
-          ? "dark"
-          : localStorage?.getItem(storageKey) === "dark"
-          ? "dark"
-          : "light";
+        const name: PaletteMode =
+          localStorage?.getItem(storageKey) === "dark"
+            ? "dark"
+            : localStorage?.getItem(storageKey) === "light"
+            ? "light"
+            : matchMedia?.("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
         ctx.setSelf(name);
       }
 
@@ -76,10 +78,7 @@ export function useTheme(name?: PaletteMode) {
 /**
  * Switches between "light" and "dark" themes.
  */
-export function useToggleTheme(
-  preferredDarkTheme: boolean = false,
-  name?: PaletteMode
-) {
+export function useToggleTheme(name?: PaletteMode) {
   return useRecoilCallback(
     (ctx) => () => {
       ctx.set(
