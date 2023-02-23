c5ecc206e109f680eefb51568f5c5fd2c1aaf7e3
GLEF1X <glebgar567@gmail.com>
2023-02-22 20:02:21 -0500

    [developer experience]: add `react-query-devtools`

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -5,12 +5,28 @@ import { ApiProvider } from "@/services/api/index.js";
 import { queryClient } from "@/services/queryClient/queryClient.js";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SnackbarProvider } from "notistack";
+import {lazy, Suspense, useEffect, useState} from "react";
 import { RouterProvider } from "react-router-dom";
 import Spinner from "./Spinner/Spinner.js";
 
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
+
 export function App(): JSX.Element {
   const theme = useTheme();
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    // @ts-expect-error
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
 
   return (
     <SnackbarProvider>
@@ -20,6 +36,12 @@ export function App(): JSX.Element {
             <ApiProvider>
               <CssBaseline />
               <RouterProvider router={router} fallbackElement={<Spinner />} />
+              <ReactQueryDevtools initialIsOpen />
+              {showDevtools && (
+                <Suspense fallback={null}>
+                  <ReactQueryDevtoolsProduction />
+                </Suspense>
+              )}
             </ApiProvider>
           </OctokitProvider>
         </QueryClientProvider>
