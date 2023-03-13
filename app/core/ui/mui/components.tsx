0062cd2ac82deb63c246d10c00e494acc0467cc6
GLEF1X <glebgar567@gmail.com>
2023-03-13 12:26:54 -0400

    [UI-kit/mui]: change link behavior, so it acts like `react-router-dom` Link

--- app/core/ui/mui/components.tsx
+++ app/core/ui/mui/components.tsx
@@ -1,5 +1,19 @@
 import { type Palette, type ThemeOptions } from "@mui/material/styles";
 
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from "react-router-dom";
+import { LinkProps } from "@mui/material";
+
+const LinkBehavior = React.forwardRef<
+  HTMLAnchorElement,
+  Omit<RouterLinkProps, "to"> & { href: RouterLinkProps["to"] }
+>((props, ref) => {
+  const { href, ...other } = props;
+  return <RouterLink ref={ref} to={href} {...other} />;
+});
+
 /**
  * Style overrides for Material UI components.
  */
@@ -16,6 +30,16 @@ export const components = (palette: Palette): ThemeOptions["components"] => ({
           boxShadow: "none",
         },
       },
+    },
+  },
+  MuiLink: {
+    defaultProps: {
+      component: LinkBehavior,
+    } as LinkProps,
+  },
+  MuiButtonBase: {
+    defaultProps: {
+      LinkComponent: LinkBehavior,
     },
   },
 
